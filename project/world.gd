5315c585ab5516ed32a3773ab07656f765d2789e
Wright <brooke.wright@bsu.edu>
2025-01-25 12:42:15 -0500

    Add ammo bar to world

--- project/world.gd
+++ project/world.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var _ammo_bar : TextureProgressBar = $Control/AmmoBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,5 +12,7 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_wizard_ammo_used() -> void:
-	pass # Replace with function body.
+func _on_wizard_ammo_used(decrease_amt) -> void:
+	print(decrease_amt)
+	_ammo_bar.value -= decrease_amt
+	
