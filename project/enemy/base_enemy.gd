3d0006a69b8ddb7768164ffa31d3feb98d56482c
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-01-25 12:03:49 -0500

    Add elements to enemy

--- project/enemy/base_enemy.gd
+++ project/enemy/base_enemy.gd
@@ -1,9 +1,18 @@
 extends CharacterBody2D
 
-
-
 const walk_speed = 400
-var enemy_health = 4
+
+@export var enemy_health = 4
+
+var element: Element = [
+	preload("res://element/normal.tres"),
+	preload("res://element/earth.tres")
+].pick_random()
+
+@onready var sprite: Sprite2D = %Sprite2D
+
+func _ready() -> void:
+	sprite.modulate = element.color
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
