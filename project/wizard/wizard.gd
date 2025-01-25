d75a4327ff74a2cb35fb9021333378794679be21
Wright <brooke.wright@bsu.edu>
2025-01-24 19:39:26 -0500

    Add hitbox to wizard

--- project/wizard/wizard.gd
+++ project/wizard/wizard.gd
@@ -1,25 +1,30 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const _SPEED := 200.0
+const _JUMP_VELOCITY := -400.0
+
+var _health := 100
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = _JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * _SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, _SPEED)
 
 	move_and_slide()
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
