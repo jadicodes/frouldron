ffe8deb17b7e7acad03d8a2a08a2bb5fcfa70344
Scott <traigh.scott@bsu.edu>
2025-01-25 17:40:09 -0500

    Implement element types to be added to the world

--- project/cauldron/cauldron.gd
+++ project/cauldron/cauldron.gd
@@ -10,11 +10,17 @@ func _ready() -> void:
 	if element == 0:
 		$Water.visible = true
 	if element == 1:
-		$Fire.visible = true
+		$Nature.visible = true
 	if element == 2:
 		$Acid.visible = true
 	if element == 3:
 		$Earth.visible = true
+	if element == 4:
+		$Electricity.visible = true
+	if element == 5:
+		$Fire.visible = true
+	if element == 6:
+		$Ice.visible = true
 	
 
 
@@ -26,13 +32,23 @@ func _process(_delta: float) -> void:
 			emit_signal("change_element",preload("res://element/normal.tres"))
 		if element == 1:
 			emit_signal("add_ammo")
-			emit_signal("change_element", "Fire.tres")
+			emit_signal("change_element", preload("res://element/nature.tres"))
 		if element == 2:
 			emit_signal("add_ammo")
 			emit_signal("change_element", preload("res://element/acid.tres"))
 		if element == 3:
 			emit_signal("add_ammo")
 			emit_signal("change_element", preload("res://element/earth.tres"))
+		if element == 4:
+			emit_signal("add_ammo")
+			emit_signal("change_element", preload("res://element/electricity.tres"))
+		if element == 5:
+			emit_signal("add_ammo")
+			emit_signal("change_element", preload("res://element/fire.tres"))
+		if element == 6:
+			emit_signal("add_ammo")
+			emit_signal("change_element", preload("res://element/ice.tres"))
+		
 	
 
 func _on_body_entered(_body: Node2D) -> void:
