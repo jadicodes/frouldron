d96da58eaa32d30f2515733207df2b35616092e4
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-01-25 17:03:01 -0500

    Update camera movement

--- project/wizard/follow_camera.gd
+++ project/wizard/follow_camera.gd
@@ -3,8 +3,11 @@ extends Camera2D
 @export var target: Node2D
 
 
+func _ready() -> void:
+	global_position.x = target.global_position.x
+
 func _process(_delta: float) -> void:
 	if not target:
 		return
 
-	global_position.x = target.global_position.x
+	global_position.x = target.global_position.x + clampf(global_position.x - target.global_position.x, -256, 256)
