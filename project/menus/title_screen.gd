fc1a298063b6a3d9f49202442bfb93068bf112d9
Wright <brooke.wright@bsu.edu>
2025-01-26 16:25:31 -0500

    Fix credit scene name issue

--- project/menus/title_screen.gd
+++ project/menus/title_screen.gd
@@ -21,4 +21,4 @@ func _on_close_tutorial_button_pressed() -> void:
 
 
 func _on_credit_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://menus/credits.tscn")
+	get_tree().change_scene_to_file("res://menus/credits_scene.tscn")
