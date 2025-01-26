cb4566381f469ae046b50161560974d7ee2828b1
SaltyTea4924 <tanner.bauserman@bsu.edu>
2025-01-26 15:00:42 -0500

    Working end screens

--- project/menus/end_screen.gd
+++ project/menus/end_screen.gd
@@ -3,11 +3,13 @@ extends Control
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
-		get_tree().change_scene_to_file("res://world/world.tscn")
+		get_tree().change_scene_to_file("res://menus/title_screen.tscn")
+	if Input.is_action_just_pressed("shoot"):
+		get_tree().quit()
 
 
 func _on_start_over_pressed() -> void:
-	get_tree().change_scene_to_file("res://world/world.tscn")
+	get_tree().change_scene_to_file("res://menus/title_screen.tscn")
 
 
 func _on_exit_button_pressed() -> void:
